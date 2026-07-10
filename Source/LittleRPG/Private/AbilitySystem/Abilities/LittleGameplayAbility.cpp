92b79d598ab4106658aa1318ab48f82aaabe4de7
Shryen <hbk9238@hotmail.com>
2026-07-10 18:15:19 +0200

    GemaplyAbility base class

---
+++ Source/LittleRPG/Private/AbilitySystem/Abilities/LittleGameplayAbility.cpp
@@ -0,0 +1,7 @@
+﻿#include "AbilitySystem/Abilities/LittleGameplayAbility.h"
+
+ULittleGameplayAbility::ULittleGameplayAbility()
+{
+	InstancingPolicy = EGameplayAbilityInstancingPolicy::InstancedPerActor;
+	NetExecutionPolicy = EGameplayAbilityNetExecutionPolicy::LocalPredicted;
+}
